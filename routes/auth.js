c3c8e96c2430607cbfb7848d8e853f5bdc798678
Valdir Silva <valdir.ti@gmail.com>
2022-10-14 07:28:53 -0300

    feat: register user

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,15 @@
 const router = require("express").Router();
-const CryptoJS = require("crypto");
+const CryptoJS = require("crypto-js");
 
 const User = require("../models/User");
 
 //REGISTER
 router.post("/register", async (req, res) => {
+
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: CryptoJS.AES.encrypt(
-      req.body.password,
-      process.env.SECRET_KEY
-    ).toString(),
+    password: CryptoJS.AES.encrypt(req.body.password, process.env.SECRET_KEY).toString()
   });
 
   try {
