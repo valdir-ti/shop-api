522800f7904dc29c283ff5fe7a1c5ba12504b9d6
Valdir Silva <valdir.ti@gmail.com>
2022-10-14 08:24:58 -0300

    feat: login user

--- routes/auth.js
+++ routes/auth.js
@@ -18,10 +18,34 @@ router.post("/register", async (req, res) => {
       savedUser,
     });
   } catch (error) {
-    res.status(400).json({
+    res.status(401).json({
       message: error.message,
     });
   }
 });
+
+//LOGIN
+router.post("/login", async (req, res) => {
+
+  try {
+    const user = await User.findOne({username: req.body.username})
+    !user && res.status(401).json({message: "Wrong username or password"})
+
+    const hashedPassword = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY)
+
+    const passwordHash = hashedPassword.toString(CryptoJS.enc.Utf8)
+    passwordHash !== req.body.password && res.status(401).json({message: "Wrong username or password"})
+
+    const { email, username, _id, isAdmin } = user
+
+    res.status(200).json({_id, username, email, isAdmin});
+
+  } catch (error) {
+    res.status(401).json({
+      message: error.message,
+    });
+  }
+
+})
 
 module.exports = router;
